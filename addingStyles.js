6dea46126f5c3327844b12813f3f75a887c54ba8
Christian Gonzalez <cgonzalez4367@gmail.com>
2023-09-24 14:41:24 -0500

    adding styles

---
+++ addingStyles.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Avatar() {
+  return <img className="avatar" src="C:\Users\chris\Desktop\React-QuickStart\avatar.jpg" alt="User Avatar" />;
+}
+
+export default Avatar;
